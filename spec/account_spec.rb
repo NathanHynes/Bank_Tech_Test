4e4f76bb3c7ae74740dfba1c4f161bbe33b405dd
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-11-18 15:51:36 +0000

    setting up second test and updating gitignore

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,5 +6,9 @@ describe 'Account' do
     it "should respond to deposit with one argument" do
       expect(account).to respond_to(:deposit).with(1).argument
     end
+
+    it "increases the account balance" do
+      expect { account.deposit(100) }.to change { account.balance }.by(100)
+    end
   end
 end
