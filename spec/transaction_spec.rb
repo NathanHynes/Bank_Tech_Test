0f4d7ed5d9beda958eb20f849b5a4ab3d392d8c8
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-11-19 09:54:17 +0000

    2nd test passing for transaction class

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -13,5 +13,10 @@ describe Transaction do
       Timecop.freeze(2019, 11, 18)
       expect(transaction.event(deposit: deposit, balance: balance_after_deposit)).to eq({ date: '18/11/2019', deposit: 200, withdrawal: nil, balance: 200 })
     end
+
+    it "creates a new transaction event when a withdrawal is made" do
+      Timecop.freeze(2019, 11, 17)
+      expect(transaction.event(withdrawal: withdrawal, balance: balance_after_withdrawal)).to eq({ date: '17/11/2019', deposit: nil, withdrawal: 50, balance: 150 })
+    end
   end
 end
