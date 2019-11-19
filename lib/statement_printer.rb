ae97b7d261f657a8e3a60a0e5315d4a052abcbed
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-11-19 11:38:03 +0000

    second test for StatementPrinter passing

--- lib/statement_printer.rb
+++ lib/statement_printer.rb
@@ -1,7 +1,39 @@
 class StatementPrinter
 
+  STATEMENT_HEADER = 'date || credit || debit || balance'
+  NEW_LINE = "\n"
+  NEW_COLUMN = "|| "
+
+  def initialize
+    @header = STATEMENT_HEADER
+    @new_line = NEW_LINE
+    @new_column = NEW_COLUMN
+  end
+
+  def print_statement(transaction_history)
+    statement = @header + @new_line
+
+    transaction_history.each do |transaction|
+      date = transaction[:date] + ' ' + @new_column
+      credit = an_integer?(transaction[:deposit]).to_s + @new_column
+      debit = an_integer?(transaction[:withdrawal]).to_s + @new_column
+      balance = money_format(transaction[:balance])
+      statement += date + credit + debit + balance + @new_line
+    end
+    return statement.chomp("\n")
+  end
+
   def money_format(money)
     '%.2f' % money
   end
 
+  private
+
+  def an_integer?(money)
+    if money.is_a? Integer
+      return money_format(money) + ' '
+    end
+
+    return nil
+  end
 end
--- spec/statement_printer_spec.rb
+++ spec/statement_printer_spec.rb
@@ -3,10 +3,25 @@ require 'timecop'
 
 describe 'StatementPrinter' do
   let(:printer) { StatementPrinter.new }
+
+  let(:transaction_history) { [
+    { date: '14/01/2019', deposit: nil, withdrawal: 500, balance: 2500 },
+    { date: '13/01/2019', deposit: 2000, withdrawal: nil, balance: 3000 },
+    { date: '10/01/2019', deposit: 1000, withdrawal: nil, balance: 1000 }
+  ]
+  }
+  let(:expected_output) { "date || credit || debit || balance\n14/01/2019 || || 500.00 || 2500.00\n13/01/2019 || 2000.00 || || 3000.00\n10/01/2019 || 1000.00 || || 1000.00" }
+
   describe '#money_format' do
     it "returns the amount of money to two decimal places" do
       amount = 200
       expect(printer.money_format(amount)).to eq '200.00'
+    end
+  end
+
+  describe '#print_statement' do
+    it "returns transaction history in a readable format" do
+      expect(printer.print_statement(transaction_history)).to eq expected_output
     end
   end
 end
